83b658d1e787b521432552d6e2c05f5941a970ee
yarik <yarikshtoda1988@gmail.com>
2022-12-02 19:51:07 +0300

    Билет 2 задача

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 
 public class Main {
+ public static class Arr{
+     public Boolean Polindrome (String first) {
+         String second = "";
+         String third = first.replace(" ", " ");
+         char[]  got =  third.toCharArray();
+         char[] got2 = new char[got.length];
+         for (int i = got.length-1; i >= 0;) {
+             for (int j = 0; j < got2.length; j++) {
+                 got2[j] = got[i];
+                 i--;
+                 second += got2[j];
+             }
+         }
+         System.out.println(second);
+         System.out.println(third);
+         return second.equalsIgnoreCase(third);
+     }
 
+     public static void main(String args[]) {
+         Arr third = new Arr();
+         System.out.println(third.Polindrome("Казак"));
+     }
+ }
 }
 
 
